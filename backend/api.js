e9ab316ab057abf2815c58e62b14e5e88d3c80bb
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-09-20 15:12:41 +0800

    included connection with mysql database
    change the database name, we could standardise it to "LJPS"

--- backend/api.js
+++ backend/api.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database : 'spm_project'
+
+  });
+
+
+
+connection.connect((err) => {
+    if (err) {
+      console.log("Error occurred", err);
+    } else {
+      console.log("Connected to MySQL Server");
+    }
+});
 
 router.get('/test', function (req, res) {
     console.log('test')
