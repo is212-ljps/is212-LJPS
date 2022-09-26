d5d1a4f00b803ce048ea5493f7c48a7cc221d3e8
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-09-26 13:44:26 +0800

    removed bootstrap at index.js

--- ljps-frontend/pages/index.js
+++ ljps-frontend/pages/index.js
@@ -7,19 +7,17 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-          crossOrigin="anonymous"
-        />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+
 
         <p className={styles.description}>
           Get started by editing{" "}
@@ -70,11 +68,7 @@ export default function Home() {
         </a>
       </footer>
 
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
-        crossOrigin="anonymous"
-      ></Script>
+
     </div>
   );
 }
