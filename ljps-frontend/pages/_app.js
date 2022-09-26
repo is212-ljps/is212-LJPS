f8d39ea8f4f53d666abab9f2373dbd024089dc75
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-09-26 13:27:38 +0800

    updated bootstrap

--- ljps-frontend/pages/_app.js
+++ ljps-frontend/pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Script from "next/script";
+import Head from "next/head";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossOrigin="anonymous"
+      ></Script>
+    </>
+  );
+
 }
 
 export default MyApp
