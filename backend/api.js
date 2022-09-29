145bafd972bf62358ad9a001f47dab43b910df1b
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-09-30 01:20:40 +0800

    added deleteskill api

--- backend/api.js
+++ backend/api.js
@@ -13,40 +13,54 @@ var connection = mysql.createConnection({
 //------------- Crud of Skills ---------------------------------------------------------
 
 router.post("/createskill", function (req, res) {
-  
-  let skillName = req.body.skillName
-  let skillDescription = req.body.skillDescription
-
-
+  let skillName = req.body.skillName;
+  let skillDescription = req.body.skillDescription;
 
   connection.connect((err) => {
-    var insert_sql = `INSERT into Skill (Skill_Name, Skill_Description, Is_Active) VALUES ('${skillName}', '${skillDescription}', TRUE );`
+    var insert_sql = `INSERT into Skill (Skill_Name, Skill_Description, Is_Active) VALUES ('${skillName}', '${skillDescription}', TRUE );`;
     connection.query(insert_sql, function (err, result) {
-      if (err){
-        if(err.code=='ER_DUP_ENTRY'){
+      if (err) {
+        if (err.code == "ER_DUP_ENTRY") {
           res.send({
-            success:false,
-            message:'Skill Name currently exist, please use a different Skill Name. '
-          })
-        }
-        else{
+            success: false,
+            message:
+              "Skill Name currently exist, please use a different Skill Name. ",
+          });
+        } else {
           res.send({
-            success: false, 
-            message:"An error occured, please try again."
-          })
+            success: false,
+            message: "An error occured, please try again.",
+          });
         }
-      }
-      else{
+      } else {
         res.send({
           success: true,
-          message:"A new skill has been successfully created!"
+          message: "A new skill has been successfully created!",
         });
-
       }
     });
-
   });
+});
 
+router.post("/deleteskill", function (req, res) {
+  let skillName = req.body.skillName;
+
+  connection.connect((err) => {
+    var delete_sql = `DELETE FROM skill WHERE Skill_Name=${skillName}  ;`;
+    connection.query(insert_sql, function (err, result) {
+      if (err) {
+        res.send({
+          success: false,
+          message: "An error occured, please try again ",
+        });
+      } else {
+        res.send({
+          success: true,
+          message: "",
+        });
+      }
+    });
+  });
 });
 
 //
