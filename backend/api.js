38a1c42c6a762c2532d7e7b0d1f0604d478096cf
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-09-29 23:50:31 +0800

    updated createrole api

--- backend/api.js
+++ backend/api.js
@@ -13,9 +13,10 @@ var connection = mysql.createConnection({
 router.post("/createrole", function (req, res) {
   let roleName = req.body.roleName;
   let roleDescription = req.body.roleDescription;
+  let department = req.body.department;
 
   connection.connect((err) => {
-    var insert_sql = `INSERT into job_role (Job_Role_Name, Job_Role_Description, Is_Active) VALUES ('${roleName}', '${roleDescription}', TRUE );`;
+    var insert_sql = `INSERT into job_role (Job_Role_Name, Job_Role_Description, Job_Department, Is_Active) VALUES ('${roleName}', '${roleDescription}', '${department}' , TRUE );`;
     connection.query(insert_sql, function (err, result) {
       if (err) {
         if (err.code == "ER_DUP_ENTRY") {
