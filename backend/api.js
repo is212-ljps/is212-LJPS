094607e27c9bb8f10c54c5ecbc70a78a8a136e66
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-09-29 10:50:02 +0800

    included api to create role

--- backend/api.js
+++ backend/api.js
@@ -1,31 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database : 'spm_project'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "ljps_db",
+});
 
+router.post("/createrole", function (req, res) {
+  let roleName = req.body.roleName;
+  let roleDescription = req.body.roleDescription;
+
+  connection.connect((err) => {
+    var insert_sql = `INSERT into job_role (Job_Role_Name, Job_Role_Description, Is_Active) VALUES ('${roleName}', '${roleDescription}', TRUE );`;
+    connection.query(insert_sql, function (err, result) {
+      if (err) {
+        if (err.code == "ER_DUP_ENTRY") {
+          res.send({
+            success: false,
+            message:
+              "Role Name currently exist, please use a different Skill Name. ",
+          });
+        } else {
+          res.send({
+            success: false,
+            message: "An error occured, please try again.",
+          });
+        }
+      } else {
+        res.send({
+          success: true,
+          message: "A new role has been successfully created!",
+        });
+      }
+    });
   });
-
-
-
-connection.connect((err) => {
-    if (err) {
-      console.log("Error occurred", err);
-    } else {
-      console.log("Connected to MySQL Server");
-    }
 });
-
-router.get('/test', function (req, res) {
-    console.log('test')
-    res.sendStatus(200)
-});
-
-
 
 module.exports = router;
