5faba804a3bd0fda615b793b62a0b6eaeed12c13
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-09-30 20:04:57 +0800

    fixed khye chun's modal bug

--- ljps-frontend/pages/_app.js
+++ ljps-frontend/pages/_app.js
@@ -1,28 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import Script from "next/script";
 import Head from "next/head";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossOrigin="anonymous"/>
-
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />
       </Head>
 
       <Component {...pageProps} />
 
       <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossOrigin="anonymous"
-      ></Script>
+      />
+        
     </>
   );
-
 }
 
-export default MyApp
+export default MyApp;
--- ljps-frontend/pages/skill/deleteskillbutton.js
+++ ljps-frontend/pages/skill/deleteskillbutton.js
@@ -1,23 +1,22 @@
 import axios from "axios";
 import React from "react";
 import { useState, useRef, useEffect } from "react";
-import { validateLength } from "../../util/validation/index";
 
 // skillTitle will be passed as a prop to delete skill
 export default function DeleteSkillButton({ skillName }) {
   const modal = useRef();
   const toast = useRef();
+  const closeButton = useRef();
 
   const [errorMessage, setErrorMessage] = useState("");
 
+  // remove later
   let testData = "Software";
   function handleSubmit(e) {
-    e.preventDefault();
-    setErrorMessage("")
+    setErrorMessage("");
 
     var myToast = new bootstrap.Toast(toast.current);
-    var myModal = new bootstrap.Modal(modal.current);
-
+    var myModal = bootstrap.Modal.getInstance(modal.current)
 
     axios
       .post("http://localhost:8080/api/deleteskill", {
@@ -25,20 +24,17 @@ export default function DeleteSkillButton({ skillName }) {
         skillName: testData,
       })
       .then(function (response) {
-        console.log(response.data)
+        console.log(response.data);
         if (response.data.success) {
-          myToast.show();
-          myModal.hide();
-
-        }
-        else{
-            setErrorMessage(response.data.message)
+          myModal.hide()
+          myToast.show()
+        } else {
+          setErrorMessage(response.data.message);
         }
       })
       .catch(function (error) {
         console.log(error);
       });
-
   }
 
   useEffect(() => {
@@ -64,27 +60,31 @@ export default function DeleteSkillButton({ skillName }) {
         </div>
       </div>
 
-      
-
       <div
-        className="modal fade"
+        className="modal"
         id="deleteSkillModal"
         tabIndex="-1"
         aria-labelledby="deleteSkillModalLabel"
+        data-backdrop="static"
         ref={modal}
       >
         <div className="modal-dialog">
           <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title w-100 text-center" id="exampleModalLabel">
-              Delete {`${testData}`}
-            </h5>
-          </div>
+            <div className="modal-header">
+              <h5
+                className="modal-title w-100 text-center"
+                id="exampleModalLabel"
+              >
+                Delete {`${testData}`}
+              </h5>
+            </div>
             <div className="modal-body text-center p-4">
               Are you sure you want to delete this skill ?
             </div>
 
-            {errorMessage.length>0 && <p className="text-danger text-center">{errorMessage}</p>}
+            {errorMessage.length > 0 && (
+              <p className="text-danger text-center">{errorMessage}</p>
+            )}
 
             <div className="modal-footer d-flex justify-content-center">
               <button
@@ -98,29 +98,29 @@ export default function DeleteSkillButton({ skillName }) {
                 type="button"
                 className="btn btn-danger"
                 data-bs-dismiss="modal"
+                ref={closeButton}
               >
                 No
               </button>
             </div>
           </div>
         </div>
-
       </div>
 
       <div
-          className="toast position-fixed bottom-0 end-0 p-2 m-4 text-white bg-success"
-          ref={toast}
-          role="alert"
-          aria-live="assertive"
-          data-bs-autohide="true"
-          aria-atomic="true"
-        >
-          <div className="d-flex ">
-            <div className="toast-body">
-              A skill has been successfully deleted
-            </div>
+        className="toast position-fixed bottom-0 end-0 p-2 m-4 text-white bg-success"
+        ref={toast}
+        role="alert"
+        aria-live="assertive"
+        data-bs-autohide="true"
+        aria-atomic="true"
+      >
+        <div className="d-flex ">
+          <div className="toast-body">
+            A skill has been successfully deleted
           </div>
         </div>
+      </div>
     </div>
   );
 }
