309068e0f7aad5affa605a1228fa9bae7ebfd664
liew khyechun <khyechun@gmail.com>
2022-10-02 19:05:00 +0800

    LJPS-9 Added view roles page

---
+++ ljps-frontend/pages/role/index.js
@@ -0,0 +1,58 @@
+import React, { useCallback, useEffect, useState } from "react"
+import axios from "axios"
+
+export default function JobRolesPage() {
+    const [roles, setRoles] = useState([])
+
+    useEffect(() => {
+        onRolesUpdate()
+    }, [])
+
+    const onRolesUpdate = useCallback(() => {
+        axios.get('http://localhost:8080/api/roles').then(res => {
+            setRoles(res.data.data)
+        })
+    }, [])
+
+    return <div className="container-fluid">
+        <table className="table table-borderless">
+            <thead>
+                <tr className=" rounded">
+                    <th scope="col" style={{ ...styles.tableHeaderBg, ...styles.tableBorderRadiusLeft }}>Role ID</th>
+                    <th scope="col" style={{ ...styles.tableHeaderBg }}>Role Name</th>
+                    <th scope="col" style={{ ...styles.tableHeaderBg }}>Role Description</th>
+                    <th scope="col" style={{ ...styles.tableHeaderBg }}>Role Department</th>
+                    <th scope="col" style={{ ...styles.tableHeaderBg, ...styles.tableBorderRadiusRight }}></th>
+                </tr>
+            </thead>
+            <tbody>
+                {roles?.map((role, index) => {
+                    return <tr key={index}>
+                        <th scope="row">{role.Job_Role_ID}</th>
+                        <td>{role.Job_Role_Name}</td>
+                        <td>{role.Job_Role_Description}</td>
+                        <td>{role.Job_Department}</td>
+                        <td>
+                            <button className="btn btn-light mx-1">Edit</button>
+                            <button className="btn btn-light mx-1">Delete</button>
+                        </td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
+    </div>
+}
+
+const styles = {
+    tableHeaderBg: {
+        background: "#F0F0F0"
+    },
+    tableBorderRadiusLeft: {
+        borderTopLeftRadius: "10px",
+        borderBottomLeftRadius: "10px"
+    },
+    tableBorderRadiusRight: {
+        borderTopRightRadius: "10px",
+        borderBottomRightRadius: "10px"
+    }
+}
