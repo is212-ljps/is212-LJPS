4722d6f7b6859a97e48fca40ea043b258f2c7d81
liew khyechun <khyechun@gmail.com>
2022-10-02 18:05:13 +0800

    LJPS-14 Added view skills skeleton

---
+++ ljps-frontend/pages/skill/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+
+export default function skillsPage() {
+    const [skills, setSkills] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/skills').then(res => {
+            setSkills(res.data.data)
+        })
+    }, [])
+
+    return <div className="container-fluid">
+        <table className="table table-borderless">
+            <thead>
+                <tr className=" rounded">
+                    <th scope="col" style={{ ...styles.tableHeaderBg, ...styles.tableBorderRadiusLeft }}>Skill ID</th>
+                    <th scope="col" style={{ ...styles.tableHeaderBg }}>Skill Name</th>
+                    <th scope="col" style={{ ...styles.tableHeaderBg }}>Skill Description</th>
+                    <th scope="col" style={{ ...styles.tableHeaderBg, ...styles.tableBorderRadiusRight }}></th>
+                </tr>
+            </thead>
+            <tbody>
+                {skills?.map((skill) => {
+                    return <tr>
+                        <th scope="row">{skill.Skill_ID}</th>
+                        <td>{skill.Skill_Name}</td>
+                        <td>{skill.Skill_Description}</td>
+                        <td>
+                            <button className="btn btn-light mx-1">Edit</button>
+                            <button className="btn btn-light mx-1">Delete</button>
+                        </td>
+                    </tr>
+                })}
+
+            </tbody>
+        </table>
+    </div>
+}
+
+const styles = {
+    tableHeaderBg: {
+        background: "#F0F0F0"
+    },
+    tableBorderRadiusLeft: {
+        borderTopLeftRadius: "10px",
+        borderBottomLeftRadius: "10px"
+    },
+    tableBorderRadiusRight: {
+        borderTopRightRadius: "10px",
+        borderBottomRightRadius: "10px"
+    }
+}
