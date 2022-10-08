698c5a8b4b917d5d6714effd1b856a35f1723c4c
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-10-08 16:56:24 +0800

    LJPS-31 Update Front End UI

--- ljps-frontend/pages/learning-journey/view-skills/index.js
+++ ljps-frontend/pages/learning-journey/view-skills/index.js
@@ -1,34 +1,52 @@
-import React , { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Image from "next/image";
+
 
 export default function ViewSkills() {
   var router = useRouter();
   var roleID = router.query["selectedRole"];
 
-  const [roleDescription, setRoleDescription] = useState("")
-  const [ roleName,  setRoleName ] = useState("")
+  const [roleDescription, setRoleDescription] = useState("");
+  const [roleDepartment, setRoleDepartment] = useState("");
+  const [roleName, setRoleName] = useState("");
 
   // get role id and description based on role id
 
-  const url = `http://localhost:8080/api/roles/roledetails/` + roleID;
-  const axiosFn = axios.get;
-  axiosFn(url)
-    .then(function (response) {
-      if (response.data.success) {
-        console.log(response.data)
-
-      } else {
-
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  useEffect(() => {
+    if (roleID) {
+      const url = `http://localhost:8080/api/roles/roledetails/` + roleID;
+      const axiosFn = axios.get;
+      axiosFn(url)
+        .then(function (response) {
+          if (response.data.success) {
+            setRoleDescription(response.data.data[0].Job_Role_Description);
+            setRoleDepartment(response.data.data[0].Job_Department);
+            setRoleName(response.data.data[0].Job_Role_Name);
+          } else {
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }, []);
 
   return (
     <div>
-      <h1> {roleID} </h1>
+      <div className="row m-4">
+        <div className="col-md-5 col-sm-12 d-flex justify-content-center align-items-center">
+          <h3 className="p-5">
+            {" "}
+            Select a skill to kickstart your Learning Journey as a <span className="text-primary fw-bold">{roleName}</span>
+          </h3>
+        </div>
+
+        <div className="col-md-7 col-sm-12 d-flex justify-content-center">
+          <Image src="/view-skill-learner.svg" height={350} width={350} />
+        </div>
+      </div>
     </div>
   );
 }
