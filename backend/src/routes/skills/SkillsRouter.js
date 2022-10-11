4242a6c254886259ab79d9e6c88e347044d74a26
liew khyechun <khyechun@gmail.com>
2022-10-12 01:21:29 +0800

    Fix missing database connection file

--- backend/src/routes/skills/SkillsRouter.js
+++ backend/src/routes/skills/SkillsRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-var connection = require('../../../database/database')
+var connection = require('../../../database')
 
 
 router.get('/', (req, res) => {
