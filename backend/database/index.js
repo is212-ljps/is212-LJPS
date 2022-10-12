a63030d552f931c8a13829509593703b56840430
liew khyechun <khyechun@gmail.com>
2022-10-12 12:37:28 +0800

    Added checking for db pw in env

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,8 +1,9 @@
 var mysql = require("mysql");
+
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: process.env.PASSWORD ? process.env.PASSWORD : '',
   database: "ljps_db",
 });
 
