523424973f63535a65ccbe018cb861b12a8d02c3
Caleb Shawn <caleb.ng.2020@scis.smu.edu.sg>
2022-10-12 12:46:47 +0800

    LJPS-33 change SQL statements
    Change the SQL statements in CoursesRouter

--- backend/src/routes/courses/CoursesRouter.js
+++ backend/src/routes/courses/CoursesRouter.js
@@ -5,12 +5,7 @@ var connection = require('../../../database/database')
 
 router.get('/', (req, res) => {
   connection.connect(err => {
-<<<<<<< Updated upstream
     const getCourses = `SELECT * FROM course`
-=======
-    // Changed the SQL statement from Is_Active = True to Course_status='active'
-    const getCourses = `SELECT * FROM course WHERE Course_Status='active'`
->>>>>>> Stashed changes
     connection.query(getCourses, (err, result) =>{
       if (err) {
         res.send({
@@ -31,10 +26,6 @@ router.get('/', (req, res) => {
 router.get('/skill/:skillID', (req, res) => {
   let skillId = req.params.skillID
   connection.connect(err => {
-<<<<<<< Updated upstream
-=======
-    // Change the SQL statement to include the WHERE statement Course_Status='Active'
->>>>>>> Stashed changes
     const getCoursesBasedOnSkill = `SELECT * FROM course WHERE Course_Status="Active" AND Course_ID in (SELECT Course_ID FROM course_skill WHERE Skill_ID=${skillId});`
     connection.query(getCoursesBasedOnSkill, (err, result) =>{
       if (err) {
