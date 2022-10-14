0b7f724cf2a4a1ff832aa2ef0737b2655a7cc57e
liew khyechun <khyechun@gmail.com>
2022-10-15 02:11:51 +0800

    LJPS-33 Added responsiveness

--- ljps-frontend/pages/learning-journey/view-skills/view-courses/index.js
+++ ljps-frontend/pages/learning-journey/view-skills/view-courses/index.js
@@ -7,7 +7,7 @@ export default function ViewCourses() {
   var router = useRouter();
   var skillID = router.query["selectedSkill"];
   const toast = useRef();
-  
+
 
   const [skillDetails, setSkillDetails] = useState({
     skillName: "",
@@ -24,7 +24,6 @@ export default function ViewCourses() {
       axiosFn(url)
         .then(function (response) {
           if (response.data.success) {
-            console.log(response)
             let newSkillDetails = {
               skillName: response.data.data[0].Skill_Name,
               skillDescription: response.data.data[0].Skill_Description,
@@ -54,10 +53,10 @@ export default function ViewCourses() {
 
   const toggleButton = (e) => {
     var id = e.target.id;
-    if (!selectedCourses.includes(id)){
-      setSelectedCourses(()=> [...selectedCourses, id])
+    if (!selectedCourses.includes(id)) {
+      setSelectedCourses(() => [...selectedCourses, id])
     } else {
-      setSelectedCourses((currentCourses)=> currentCourses.filter((course)=> course != id))
+      setSelectedCourses((currentCourses) => currentCourses.filter((course) => course != id))
     }
   };
 
@@ -92,120 +91,36 @@ export default function ViewCourses() {
         </div>
       </div>
 
-      {/* <div className="row mx-4">
-        {courses.length > 0 &&
-          courses.map((course) => (
-            <div
-              className="col-6 col-md-3"
-              style={{ overflowWrap: "break-word" }}
-            >
-              <button
-                type="button"
-                id={course.Course_ID}
-                key={course.Course_ID}
-                className={
-                  selectedCourses.includes(course.Course_ID)
-                    ? "btn btn-outline-primary my-3 w-100 active"
-                    : "btn btn-outline-primary my-3 w-100"
-                }
-                onClick={toggleButton}
-              >
-                {course.Course_Name} 
-              </button>
-            </div>
-          ))}
-      </div> */}
-
-      {/* <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Course ID</th>
-            <th scope="col">Course Name</th>
-            <th scope="col">Course Description</th>
-            <th scope="col">Course Category</th>
-            <th scope="col"> Select </th>
-          </tr>
-        </thead>
-
-        <tbody> {courses.length > 0 && courses.map((course)=>(
-          <tr>
-            <th>{course.Course_ID}</th>
-            <th> {course.Course_Name}</th>
-            <th> {course.Course_Desc}</th>
-            <th> {course.Course_Category}</th>
-            <th> <input type={"checkbox"} id={course.Course_ID} key={course.Course_ID} className={selectedCourses.includes(course.Course_ID)} onClick={toggleButton}></input> </th>
-
-          </tr>
-
-        ))}
-
-        </tbody>
-      </table> */}
-
-      
-
-
-      {/* <div className="container mx-4"> {courses.length > 0 && courses.map((course)=>(
-        <div className="row">
-        <div className="col-5">
-          <div className="card">
-            <div className="card-header bg-info">Course ID: {course.Course_ID}</div>
-            <div className="card-body"></div>
-              <div className="row">
-                <div className="col-sm-10"> <b>{course.Course_Name}</b></div> 
-                <div className="col-sm-2"> {course.Course_Category} </div>
-              </div>
-
-              <div className="row">
-                <div className="col-sm-10">
-                  <p>{course.Course_Desc}</p>
-                </div>
-                
-                <div className="col-sm-2">
-                <input type={"checkbox"} id={course.Course_ID} key={course.Course_ID} className={selectedCourses.includes(course.Course_ID)} onClick={toggleButton}></input>
-                </div>
-              </div>
-      
-              <small>Course taught by All-in-one</small>
-
-          </div>
-        </div>
-
-      </div>
-        
-      )) }
-      </div> */}
-
-      <div className="container mx-6 px-1">
-        <div className="row"> {courses.length > 0 && courses.map((course)=>(
-          <div className="col-sm-4">
+      <div className="container mx-6 px-3">
+        <div className="row"> {courses.length > 0 && courses.map((course) => (
+          <div className="col-12 col-md-6 col-xl-4">
             <div className="card mt-2">
-              <div className="card-header" style={{"backgroundColor":"black", color:"white"}}> <b>{course.Course_Name}</b> </div>
+              <div className="card-header bg-primary text-light"> <b>{course.Course_Name}</b> </div>
               <div className="card-body"></div>
               <div className="row">
-                <div className="col-4">
+                <div className="col-5">
                   <p className="mx-3">{skillDetails.skillName}</p>
                 </div>
-                <div className="col-8">
-                  <div className="col-3 badge bg-light text-black" style={{"marginLeft":190}}> {course.Course_Category} </div>
-                </div>
-                
-              </div>
-              <div className="row">
-                <div className="col-9">
-                  <div className="col-9 mx-3"> <b>Course ID: {course.Course_ID}</b></div> 
+                <div className="col-7 px-4" align="right">
+                  <div className=" badge bg-light text-black"> {course.Course_Category} </div>
                 </div>
 
-                <div className="col-3">
-                  <span style={{"marginLeft":25, "size":1}}>Add</span>
-                </div> 
-                
-              
+              </div>
+              <div className="row pe-3">
+                <div className="col-9">
+                  <div className="col-9 mx-3"> <b>Course ID: {course.Course_ID}</b></div>
+                </div>
+
+                <div className="col-3" align="right">
+                  <label style={{ cursor: "pointer" }} for={course.Course_ID}>Add</label>
+                </div>
+
+
               </div>
               <div className="row mx-1">
                 <div className="col-10"> <p>{course.Course_Desc}</p> </div>
-                <div className="col-2"> 
-                <div style={{"marginLeft":8}}> <input type={"checkbox"} id={course.Course_ID} key={course.Course_ID} className={selectedCourses.includes(course.Course_ID)} onClick={toggleButton}></input></div>
+                <div className="col-2" align="right">
+                  <div > <input type={"checkbox"} id={course.Course_ID} key={course.Course_ID} className={selectedCourses.includes(course.Course_ID)} onClick={toggleButton}></input></div>
                 </div>
               </div>
 
@@ -215,15 +130,8 @@ export default function ViewCourses() {
         ))}
 
         </div>
-
+        {/* [LJPS-35] Add button here */}
       </div>
-
-
-
-
-
-
     </div>
-  
   );
 }
