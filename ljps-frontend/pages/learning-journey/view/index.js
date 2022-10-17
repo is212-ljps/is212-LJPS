8bee54fc81435c9f76f2ab3dd6246b739d1c795e
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-10-17 20:49:23 +0800

    LJPS-36 Added instructions on what to do

--- ljps-frontend/pages/learning-journey/view/index.js
+++ ljps-frontend/pages/learning-journey/view/index.js
@@ -2,39 +2,38 @@ import React, { useEffect, useState, useCallback } from "react";
 import Image from "next/image";
 import axios from "axios";
 
-
 export default function ViewLearningJourneys() {
-
   const [learningJourney, setLearningJourney] = useState([]);
-  
+
   useEffect(() => {
     onLearningJourneyUpdate();
   }, []);
 
-
   const onLearningJourneyUpdate = useCallback(() => {
     // currently staffID is hardcoded
-    axios.get("http://localhost:8080/api/learning-journey/130002").then((res) => {
-      setLearningJourney(res.data.data);
-    });
+    axios
+      .get("http://localhost:8080/api/learning-journey/130002")
+      .then((res) => {
+        setLearningJourney(res.data.data);
+      });
   }, []);
 
-
-  console.log(learningJourney)
-
-
+  console.log(learningJourney);
 
   return (
     <div>
       <div className="row p-3">
         <div className="col-md-6 d-flex justify-content-center align-items-center">
           <h3 className="fw-bold"> My Learning Journeys</h3>
+          {/* show number of learning journeys */}
         </div>
 
         <div className="col-md-6 d-flex justify-content-center align-items-center">
           <Image src="/view-learning-journey.svg" height={350} width={350} />
         </div>
       </div>
+
+      <div className="row">{/* display all learning journeys here */}</div>
     </div>
   );
 }
