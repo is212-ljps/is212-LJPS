9f11175daa9697bb4d372a5175200d7bea7d398b
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-10-21 14:01:54 +0800

    LJPS-37 updated learning journey api

--- backend/src/routes/learning-journey/LearningJourneyRouter.js
+++ backend/src/routes/learning-journey/LearningJourneyRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
 
-function learningJourneyRoutes(database){
+function learningJourneyRoutes(database) {
   const router = express.Router();
-  router.post('/', async (req, res) => {
+  router.post("/", async (req, res) => {
     let learningJourneyName = req.body.learningJourneyName;
     let staffId = req.body.staffId;
     let jobRoleId = req.body.jobRoleId;
@@ -10,63 +10,107 @@ function learningJourneyRoutes(database){
     let courses = req.body.courses;
 
     try {
-      const learningJourneyId = await database.createLearningJourney(learningJourneyName, staffId, jobRoleId);
+      const learningJourneyId = await database.createLearningJourney(
+        learningJourneyName,
+        staffId,
+        jobRoleId
+      );
       await database.createLearningJourneySkill(learningJourneyId, skillId);
       await database.createLearningJourneyCourse(learningJourneyId, courses);
       res.status(201).send({
         success: true,
-        message: "Learning Journey Created"
+        message: "Learning Journey Created",
       });
     } catch (err) {
-      console.log(err)
+      console.log(err);
       res.status(500).send({
         success: false,
         message: "An error occured, please try again ",
-      })
-    }  
-  })
-  
-  router.get('/:learningJourneyId', async (req, res) => {
-    let learningJourneyId = req.params.learningJourneyId
+      });
+    }
+  });
+
+  router.get("/:learningJourneyId", async (req, res) => {
+    let learningJourneyId = req.params.learningJourneyId;
+    let learningJourney = {};
     try {
-      
-      const result = await database.getLearningJourney(learningJourneyId);
+      // {
+      //   LearningJourneyID : 1,
+      //   Name: "",
+      //   Courses: {
+      //     cor30001 : {
+      //        name:"",
+      //        description:"",
+      //        skills: {12: {name:"Skill12"}}}
+      //   }
+      // }
+      const learningJourneyResult = await database.getLearningJourney(
+        learningJourneyId
+      );
+      learningJourney.Learning_Journey_ID =
+        learningJourneyResult[0].Learning_Journey_ID;
+      learningJourney.Learning_Journey_Name =
+        learningJourneyResult[0].Learning_Journey_Name;
+
+      const learningJourneyCoursesResult =
+        await database.getLearningJourneyCourses(learningJourneyId);
+
+      learningJourney.courses = learningJourneyCoursesResult;
+
+      const courses = learningJourneyCoursesResult.map(
+        ({ Course_ID }) => Course_ID
+      );
+
+      const coursesSkillsResult = await database.getCoursesSkills(courses);
+
+      const courseSkills = coursesSkillsResult.map(
+        ({ Skill_Name, Course_ID }) => {
+          return { Skill_Name, Course_ID };
+        }
+      );
+
+      for (let course of learningJourney.courses) {
+        for (let skill of courseSkills) {
+          if (skill.Course_ID == course.Course_ID) {
+            if (course.skills) {
+              course.skills.push(skill.Skill_Name);
+            }
+            else{
+              course.skills=[skill.Skill_Name]
+            }
+          }
+        }
+      }
+
+
       res.status(200).send({
         success: true,
-        data: result
+        data: learningJourney,
       });
     } catch (err) {
-      console.log(err)
+      console.log(err);
       res.status(500).send({
         success: false,
         message: "An error occured, please try again ",
-      })
-    }  
-  })
-
+      });
+    }
+  });
 
   router.get("/staff/:staffID", async (req, res) => {
-
-    
-    let staffID = req.params.staffID
-
+    let staffID = req.params.staffID;
     try {
-      
       const result = await database.getLearningJourneyByStaffID(staffID);
 
       res.status(200).send({
         success: true,
-        data: result
+        data: result,
       });
     } catch (err) {
       res.status(500).send({
         success: false,
         message: "An error occured, please try again ",
-      })
-    }  
-
-
-
+      });
+    }
   });
 
   return router;
