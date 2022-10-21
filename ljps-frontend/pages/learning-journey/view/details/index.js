db5c17e4d9fb16e40a6833d94c4c85114c5122f1
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-10-21 14:02:09 +0800

    LJPS-37 added api call on frontend

--- ljps-frontend/pages/learning-journey/view/details/index.js
+++ ljps-frontend/pages/learning-journey/view/details/index.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
+export default function LearningJourneyDetails() {
+  var router = useRouter();
+  var learningJourneyId = router.query["learningJourneyID"];
 
-export default function LearningJourneyDetails(){
-    var router = useRouter();
-    var learningJourneyID = router.query["learningJourneyID"];
+  const [learningJourney, setLearningJourney] = useState("");
 
-    
+  useEffect(() => {
+    if (learningJourneyId) {
+      const url = `http://localhost:8080/api/learning-journey/${learningJourneyId}`;
+      const axiosFn = axios.get;
+      axiosFn(url).then(function (response) {
+        if (response.data.success) {
+          console.log(response.data.data);
+        } else {
+        }
+      });
+    }
+  });
 
-    return(
-        <div>
-            Yo
-        </div>
-    )
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-6"></div>
+
+        <div className="col-md-6"></div>
+      </div>
+    </div>
+  );
 }
