9928d7c0a220eae2db1342819a379b1acd466de1
Lance Lim <lance.Lim.2020@scis.smu.edu.sg>
2022-10-22 08:53:11 +0800

    LJPS-43 minor UI update

--- ljps-frontend/pages/learning-journey/view-skills/view-courses/confirmation/index.js
+++ ljps-frontend/pages/learning-journey/view-skills/view-courses/confirmation/index.js
@@ -1,70 +1,77 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React from "react";
 import Image from "next/image";
-import axios from "axios";
 import Router, { useRouter } from "next/router";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function ViewConfirmation() {
-
   const checkCreateLearningJourney = () => {
     Router.push({
-        pathname: "/learning-journey"
-    })
-  }
+      pathname: "/learning-journey",
+    });
+  };
 
   const checkViewLearningJourney = () => {
     Router.push({
-        pathname: "/learning-journey/view"
-    })
-  }
-
+      pathname: "/learning-journey/view",
+    });
+  };
 
   return (
-    <div>
-
-    <div>
-        <nav className="navbar navbar-light bg-light p-3">
-            <span className="navbar-brand mb-0 h1"> All-in-one LJPS </span>
+    <div className="pb-5">
+      <div>
+        <nav className="navbar navbar-light bg-light px-3">
+          <span className="navbar-brand mb-0 h1"> All-in-one LJPS </span>
         </nav>
 
         <div className="container">
-            <div className="row p-3" style={{"marginTop":100}}>
-                <div className="col-md d-flex justify-content-center align-items-center ">
-                    <Image src="/ConfirmationLogo.jpg" height={350} width={350} />
-                </div>
+          <div className="row">
+            <div className="col-md d-flex justify-content-center align-items-center ">
+              <Player
+                src="https://assets10.lottiefiles.com/packages/lf20_lrlxrdvc.json"
+                style={{ height: '350px', width: '350px' }}
+                autoplay
+                loop
+                className="player"
+              />
             </div>
+          </div>
         </div>
 
-        <br></br>
 
         <div className="container">
-            <div className="row p-3 d-flex justify-content-center align-items-center text-center">
-                <h2>Your Learning Journey has been successfully created!</h2>
-            </div>
-
+          <div className="row my-3 d-flex justify-content-center align-items-center text-center">
+            <h3>Your Learning Journey has been successfully created!</h3>
+          </div>
         </div>
 
         <div className="container">
-            <div className="row p-3 ">
-                {/* Create Learning Journey Button */}
-                <div className="col-sm-6 d-flex justify-content-end align-items-center">
-                    <button type="button" className="btn btn-primary btn-lg" onClick={checkCreateLearningJourney}>
-                        {" "} Create Learning Journey {" "}
-                    </button>
-                </div>
-
-                {/* View Learning Journey Button */}
-                <div className="col-sm-6 d-flex justify-content-start align-items-center">
-                    <button type="button" className="btn btn-primary btn-lg" onClick={checkViewLearningJourney}>
-                        {" "} View Learning Journey {" "}
-                    </button>
-                </div>
-
-
+          <div className="row ">
+            {/* Create Learning Journey Button */}
+            <div className="col-sm-6 d-flex justify-content-center align-items-center mt-4">
+              <button
+                type="button"
+                className="btn btn-primary btn-lg w-75"
+                onClick={checkCreateLearningJourney}
+              >
+                {" "}
+                Create Learning Journey{" "}
+              </button>
             </div>
+
+            {/* View Learning Journey Button */}
+            <div className="col-sm-6 d-flex justify-content-center align-items-center mt-4">
+              <button
+                type="button"
+                className="btn btn-outline-primary btn-lg w-75"
+                onClick={checkViewLearningJourney}
+              >
+                {" "}
+                View Learning Journey{" "}
+              </button>
+            </div>
+          </div>
         </div>
-
-
+      </div>
     </div>
-  </div>
-  )
+  );
 }
