fa878eb4e9ebb6e6d5f90373f32a789023463683
liew khyechun <khyechun@gmail.com>
2022-10-24 17:55:42 +0800

    LJPS-63 Fixed Skills Modal form reset bug

--- ljps-frontend/pages/skill/index.js
+++ ljps-frontend/pages/skill/index.js
@@ -27,7 +27,7 @@ export default function SkillsPage() {
     })
 
     return <div className="container-fluid">
-        <SkillModal selectedSkill={selectedSkill} onSkillsUpdate={onSkillsUpdate} />
+        <SkillModal selectedSkill={selectedSkill} onSkillsUpdate={onSkillsUpdate} resetSelectedSkill={resetSelectedSkill}/>
         <div className="ml-auto my-2"><CreateSkillButton onSkillsUpdate={onSkillsUpdate} resetSelectedSkill={resetSelectedSkill}  /></div>
         <table className="table table-borderless">
             <thead>
