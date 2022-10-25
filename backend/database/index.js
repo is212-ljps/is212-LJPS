e7a9eff944f4f3c26a5ff3ffc3dd58dc32db2fcb
Liye Ng <liye1998@hotmail.com>
2022-10-25 10:46:15 +0800

    Revert "LJPS-21 Update getAllRoles endpoint"
    This reverts commit 9660da8be0b69ca9bfbfab06570698a76c726525.

--- backend/database/index.js
+++ backend/database/index.js
@@ -204,7 +204,7 @@ exports.deleteRoleById = async (roleID) => {
 }
 
 exports.getAllRoles = async () => {
-  const getRoles = `SELECT * FROM job_role`
+  const getRoles = `SELECT * FROM job_role WHERE Is_Active=TRUE`
   try {
     const result = await promiseQuery(getRoles)
     return result
