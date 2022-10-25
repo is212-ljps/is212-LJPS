5c1f88bbc7640198cc87f9622d6ca3fc3c2bafd3
Caleb Shawn <caleb.ng.2020@scis.smu.edu.sg>
2022-10-25 23:15:06 +0800

    Update app.js
    added in get.items and added nav bar

--- ljps-frontend/pages/_app.js
+++ ljps-frontend/pages/_app.js
@@ -2,43 +2,136 @@ import "../styles/globals.css";
 import Script from "next/script";
 import Head from "next/head";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { useRouter } from "next/router";
+import { useEffect, useState } from 'react'
 
 
 function MyApp({ Component, pageProps }) {
   // Get Role details here
-  const role = window.localStorage("userDetails");
+ 
+  var role = ""
+  if (typeof window !== 'undefined') {
+    role = JSON.parse(localStorage.getItem('userDetails')).role;
+  }
+
   console.log(role)
+  // const [role, setRole] = useState()
+  // if( typeof window !== 'undefined'){
+  //   useEffect(() => setRole(JSON.parse(localStorage.getItem('userDetails').role)),[]);
+  // }
+  
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossOrigin="anonymous"/>
-
       </Head>
 
+      <div>
+        {role == "User" && 
+        <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
+          <a className="navbar-brand" href="/learning-journey"> <h4>All-in-One LJPS</h4> </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/learning-journey"> <strong>Create Learning Journey</strong></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/learning-journey/view"> <strong> View My Learning Journey </strong></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link"> {role}</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        }
+
+        {role == "Manager" &&
+        <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
+          <a className="navbar-brand" href="/learning-journey"> <h4>All-in-One LJPS</h4> </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/roles"> <strong>Roles</strong></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/skill"> <strong>Skills</strong></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/learning-journey"> <strong>Create Learning Journey</strong></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/learning-journey/view"> <strong> View My Learning Journey </strong></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">{role}</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        }
+
+        {role == "Admin" &&
+        <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
+          <a className="navbar-brand" href="/learning-journey"> <h4>All-in-One LJPS</h4> </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/roles"> <strong>Roles</strong></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/skill"> <strong>Skills</strong></a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/learning-journey"> <strong>Create Learning Journey</strong></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/learning-journey/view"> <strong> View My Learning Journey </strong></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">{role}</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        }
+      </div>
+
+
+
       {/* Navbar here */}
-      <nav class="navbar navbar-expand-lg navbar-light bg-white m-3">
-        <a class="navbar-brand" href="/learning-journey"> <h4>All-in-One LJPS</h4> </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      {/* <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
+        <a className="navbar-brand" href="/learning-journey"> <h4>All-in-One LJPS</h4> </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="/learning-journey"> <strong>Create Learning Journey</strong></a>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="/learning-journey"> <strong>Create Learning Journey</strong></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/learning-journey/view"> <strong> View Learning Journey </strong></a>
+            <li className="nav-item">
+              <a className="nav-link" href="/learning-journey/view"> <strong> View Learning Journey </strong></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">User</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">User</a>
             </li>
           </ul>
         </div>
-      </nav>
+      </nav> */}
 
       <Component {...pageProps} />
 
