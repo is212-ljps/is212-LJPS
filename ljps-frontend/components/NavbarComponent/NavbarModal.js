672d766d09fb4f1f7ae5344537b2b5654326a763
Caleb Shawn <caleb.ng.2020@scis.smu.edu.sg>
2022-10-28 15:42:22 +0800

    LJPS-46 Hydration error
    Trying to fix the hydration error

--- ljps-frontend/components/NavbarComponent/NavbarModal.js
+++ ljps-frontend/components/NavbarComponent/NavbarModal.js
@@ -1,43 +1,12 @@
-import "../styles/globals.css";
-import Script from "next/script";
-import Head from "next/head";
-import "bootstrap-icons/font/bootstrap-icons.css";
-//import React, { useState } from "react";
-import { Player } from "@lottiefiles/react-lottie-player";
-import { useRouter } from "next/router";
-import React, { useState, useEffect, useRef } from "react";
-import NavBarModal from "../components/NavbarComponent/NavbarModal"
+import React, { useCallback, useRef, useEffect, useState } from "react";
+import axios from "axios";
+import Router, { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  // Get Role details here
- 
-  // var role = ""
-  // if (typeof window !== 'undefined') {
-  //   role = JSON.parse(localStorage.getItem('userDetails')).role;
-  // }
-  const role = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('userDetails')).role : null
-  //const [role, setRole] = useState(null) 
-  //useEffect(()=> setRole(JSON.parse(localStorage.getItem('userDetails')).role), [])
+export default function NavBarModal(){
+    const role = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem('userDetails')).role : null
 
-  console.log(role)
-  // const [role, setRole] = useState()
-  // if( typeof window !== 'undefined'){
-  //   useEffect(() => setRole(JSON.parse(localStorage.getItem('userDetails').role)),[]);
-  // }
-  
-
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossOrigin="anonymous"/>
-      </Head>
-
-      <NavBarModal/>
-
-
-
-      {/* <div>
+    return (
+        <div>
         {role == "User" && 
         <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
           <a className="navbar-brand" href="/learning-journey"> <h4>All-in-One LJPS</h4> </a>
@@ -115,18 +84,7 @@ function MyApp({ Component, pageProps }) {
           </div>
         </nav>
         }
-      </div> */}
+      </div>
+    )
 
-
-      <Component {...pageProps} />
-
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
-        crossOrigin="anonymous"
-      ></Script>
-    </>
-  );
 }
-
-export default MyApp;
