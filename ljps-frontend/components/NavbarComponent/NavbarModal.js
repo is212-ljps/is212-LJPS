2e08fe4a231bb5c96c3904724b64c206b5b3a3b1
liew khyechun <khyechun@gmail.com>
2022-11-01 16:22:55 +0800

    LJPS-47 Updated navbar to use Link from next.

--- ljps-frontend/components/NavbarComponent/NavbarModal.js
+++ ljps-frontend/components/NavbarComponent/NavbarModal.js
@@ -1,96 +1,49 @@
-import React, { useCallback, useRef, useEffect, useState } from "react";
-import axios from "axios";
-import Router, { useRouter } from "next/router";
+import React from "react";
 import { store } from "../../store";
+import Link from 'next/link';
 
-export default function NavBarModal(){
+export default function NavBarModal() {
+  const role = store.role
 
-    const role = store.role
-
-    console.log(role)
-    return (
-        <div>
-
-        {role == "User" && 
-        <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/learning-journey"> <h4>All-in-One LJPS</h4> </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/learning-journey">Create Learning Journey</a>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
+      <div className="container-fluid">
+        <span className="navbar-brand">
+          <Link href="/learning-journey"> All-in-One LJPS </Link>
+        </span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <span className="nav-link active">
+                <Link aria-current="page" href="/learning-journey">Create Learning Journey</Link>
+              </span>
+            </li>
+            <li className="nav-item">
+              <span className="nav-link">
+                <Link href="/learning-journey/view">View my Learning Journey</Link>
+              </span>
+            </li>
+            {role != "User" &&
+              <>
+                < li className="nav-item">
+                  <span className="nav-link">
+                    <Link className="nav-link" href="/roles">Roles</Link>
+                  </span>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/learning-journey/view">View my Learning Journey</a>
+                  <span className="nav-link">
+                    <Link className="nav-link" href="/skills">Skills</Link>
+                  </span>
                 </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        }
-
-
-        {role == "Manager" && 
-        <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/learning-journey"><h4>All-in-One LJPS</h4></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/learning-journey">Create Learning Journey</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/learning-journey/view">View My Learning Journey</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/roles">Roles</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/Skill">Skills</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        }
-
-        {role == "Admin" && 
-        <nav className="navbar navbar-expand-lg navbar-light bg-white m-3">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/learning-journey"><h4>All-in-One LJPS</h4></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/learning-journey">Create Learning Journey</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/learning-journey/view">View My Learning Journey</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/roles">Roles</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/Skill">Skills</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        }
-
-
-      
+              </>
+            }
+          </ul>
+        </div>
       </div>
-      
-    )
+    </nav>
 
+  )
 }
